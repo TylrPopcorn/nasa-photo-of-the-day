8593f1d513a7304d7e90992862328cf56b1a788d
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-06-16 18:52:12 -0400

    Changes

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NasaStyle = styled.div`
+    img{
+        border-radius: 10px;
+    }
+
+    h3{
+        height: 40px;
+    }
+
+    transition: all 0.2s ease-in-out;
+    &:hover{
+        background-color: grey;
+
+        h3{
+            color: white;
+            font-size: 20px;
+        }
+    }
+  }
+`
 
 const NasaPhoto = (props) => {
     // console.log(props);
     return (
-        <div className="nasa-photo-wrapper">
+        <NasaStyle className="nasa-photo-wrapper">
             <h3> {props.data.title} </h3>
             <img src={props.data.hdurl} />
             <p className="explanation"> {props.data.date} </p>
-        </div>
+        </NasaStyle>
     )
 }
 
